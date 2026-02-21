eaa1872cc7cfcb597d48acb15468f6e03256e771
dineshpdk@789 <dineshpdk789@gmail.com>
2026-02-21 09:41:24 +0530

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WlBzc/Occ8A2aP8fHfos2DCpzfSqo/JMVymx03SQiHw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
